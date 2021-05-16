fb334a7a0bd9a643877a3959ddb70ab4aad09e5a
windowtrap <38334931+windowtrap@users.noreply.github.com>
2021-05-16 23:34:48 +1200

    BuildAttempt

---
+++ textTransformer.js
@@ -0,0 +1,24 @@
+function changeSelection(){
+
+  var e = document.getElementById("selector");
+  var selectedItem = e.options[e.selectedIndex].text;
+
+  document.getElementById("populator2").innerHTML = selectedItem;
+
+  console.log(selectedItem);
+
+  var url = e.options[e.selectedIndex].value;
+
+    console.log(url);
+
+
+
+  document.getElementById("img5").classList.add("glow");
+
+}
+
+function moveOn(){
+var e = document.getElementById("selector");
+//  window.location.href = e.options[e.selectedIndex].value;
+  newWindow = window.open(e.options[e.selectedIndex].value);
+}
