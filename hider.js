2f6e4083bc2c058bc2809d3bf50aa9df7fdb25dc
windowtrap <38334931+windowtrap@users.noreply.github.com>
2021-05-16 23:11:20 +1200

    NewVersion

---
+++ hider.js
@@ -0,0 +1,21 @@
+function hider(){
+
+  // var e = document.getElementById("selector");
+  // var selectedItem = e.options[e.selectedIndex].text;
+  //
+  // document.getElementById("populator2").innerHTML = selectedItem;
+  //
+  // console.log(selectedItem);
+  //
+  // var url = e.options[e.selectedIndex].value;
+  //
+  //   console.log(url);
+
+
+
+  document.getElementById("primaryDiv").classList.add("hideMe");
+    document.getElementById("primaryDiv").classList.remove("container");
+    document.getElementById("secondaryDiv").classList.remove("hideMe");
+      // document.getElementById("secondaryDiv").classList.add("container");
+
+}
