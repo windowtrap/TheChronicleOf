c156e92bae8ee0293f7898624530136584014cfc
windowtrap <38334931+windowtrap@users.noreply.github.com>
2021-05-17 23:05:32 +1200

    Update

--- hider.js
+++ hider.js
@@ -1,21 +1,18 @@
 function hider(){
 
-  // var e = document.getElementById("selector");
-  // var selectedItem = e.options[e.selectedIndex].text;
-  //
-  // document.getElementById("populator2").innerHTML = selectedItem;
-  //
-  // console.log(selectedItem);
-  //
-  // var url = e.options[e.selectedIndex].value;
-  //
-  //   console.log(url);
-
-
-
   document.getElementById("primaryDiv").classList.add("hideMe");
     document.getElementById("primaryDiv").classList.remove("container");
     document.getElementById("secondaryDiv").classList.remove("hideMe");
       // document.getElementById("secondaryDiv").classList.add("container");
 
+}
+
+
+function unfoldAbout(){
+
+  document.getElementById("primaryDiv").classList.add("hideMe");
+    // document.getElementById("primaryDiv").classList.remove("container");
+    document.getElementById("secondaryDiv").classList.add("hideMe");
+
+      document.getElementById("aboutDiv").classList.remove("hideMe");
 }
