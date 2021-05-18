b192f7caa6d1fca558c80c188a0f36b58fd1e83c
windowtrap <38334931+windowtrap@users.noreply.github.com>
2021-05-18 21:17:33 +1200

    Update

--- hider.js
+++ hider.js
@@ -1,18 +1,30 @@
-function hider(){
-
+function hider() {
   document.getElementById("primaryDiv").classList.add("hideMe");
-    document.getElementById("primaryDiv").classList.remove("container");
-    document.getElementById("secondaryDiv").classList.remove("hideMe");
-      // document.getElementById("secondaryDiv").classList.add("container");
-
+  document.getElementById("primaryDiv").classList.remove("container");
+  document.getElementById("secondaryDiv").classList.remove("hideMe");
+  document.getElementById("backIcon").classList.remove("hideMe");
 }
 
 
-function unfoldAbout(){
-
+function unfoldAbout() {
   document.getElementById("primaryDiv").classList.add("hideMe");
-    // document.getElementById("primaryDiv").classList.remove("container");
-    document.getElementById("secondaryDiv").classList.add("hideMe");
+  document.getElementById("secondaryDiv").classList.add("hideMe");
+  document.getElementById("aboutDiv").classList.remove("hideMe");
+}
 
-      document.getElementById("aboutDiv").classList.remove("hideMe");
+function unfoldArtists() {
+  document.getElementById("primaryDiv").classList.add("hideMe");
+  document.getElementById("secondaryDiv").classList.add("hideMe");
+  document.getElementById("aboutDiv").classList.add("hideMe");
+  document.getElementById("artistsDiv").classList.remove("hideMe");
+
+}
+
+function resetIt() {
+  document.getElementById("primaryDiv").classList.remove("hideMe");
+  document.getElementById("primaryDiv").classList.add("container");
+  document.getElementById("secondaryDiv").classList.add("hideMe");
+  document.getElementById("aboutDiv").classList.add("hideMe");
+  document.getElementById("artistsDiv").classList.add("hideMe");
+  document.getElementById("backIcon").classList.add("hideMe");
 }
