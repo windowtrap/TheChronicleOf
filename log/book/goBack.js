fed03f9339be5373081b18762ca84c60bf1e1123
windowtrap <38334931+windowtrap@users.noreply.github.com>
2022-01-17 22:07:02 +1300

    Book

---
+++ log/book/goBack.js
@@ -0,0 +1,22 @@
+let boolOp = 0;
+
+function expand(){
+  boolOp = !boolOp;
+
+  if (boolOp){
+    document.getElementById("text").classList.remove("textBefore");
+    document.getElementById("text").classList.add("textClicked");
+    document.getElementById("expander").classList.add("imageClicked");
+  }
+
+  if (!boolOp){
+    document.getElementById("text").classList.add("textBefore");
+    document.getElementById("text").classList.remove("textClicked");
+    document.getElementById("expander").classList.remove("imageClicked");
+  }
+}
+
+function goBack() {
+  window.history.back();
+setTimeout(hider, 1000);
+}
